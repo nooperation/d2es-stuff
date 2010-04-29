bbfe644454d73d0444ce1dd2c84500f8bfaee931
MadBrahmin <madbrahmin@gmail.com>
2010-04-29 02:38:51 +0000

    ammoGuard module added
    git-svn-id: http://d2es-stuff.googlecode.com/svn/trunk@24 5307f4c3-6ab2-341e-5f1a-034275371280

---
+++ d2hackitmodules/ammoGuard/AmmoGuard/ammoguard.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../../d2hackit/includes/D2Client.h"
+
+enum States
+{
+	STATE_IDLE = 0,
+	STATE_AMMO_FROM_EQUIP,
+	STATE_AMMO_TO_EQUIP,
+	STATE_AMMO_TO_CUBE,
+	STATE_AMMO_FROM_CUBE,
+	STATE_TRANSMUTE,
+};
+
+class AmmoGuard
+{
+public:
+	AmmoGuard();
+
+	void ReloadAmmo();
+
+	void Tick();
+	void OnItemToCursor(DWORD itemId);
+	void OnItemToCube(DWORD itemId);
+	void OnItemToEquip(DWORD itemId);
+	void Abort();
+	
+private:
+	void OnComplete();
+
+	DWORD ammoId;
+	bool inventoryOpenAtStart;
+	int ticksUntilNextCheck;
+	int currentState;
+};
