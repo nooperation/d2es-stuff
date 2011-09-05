90ce85be7e9424e935790a7d6ee6d5d48dd0fc02
MadBrahmin <madbrahmin@gmail.com>
2011-09-05 00:17:34 +0000

    Buff: Added buff project
    git-svn-id: http://d2es-stuff.googlecode.com/svn/trunk@160 5307f4c3-6ab2-341e-5f1a-034275371280

---
+++ d2hackitmodules/buffMe/buffMe/main.cpp
@@ -0,0 +1,129 @@
+#include "../../d2hackit/includes/ClientCore.cpp"
+#include "BuffMe.h"
+
+BuffMe buff;
+bool hotkeyEnabled = true;
+
+int tickRate = 100;
+int tickCount = 0;
+bool isShowingSelectedSkill = false;
+
+CLIENTINFO
+(
+	0,1,
+	"",
+	"",
+	"buffMe",
+	""
+)
+
+
+BOOL PRIVATE Buff(char** argv, int argc)
+{
+	buff.Start();
+	
+	return TRUE;
+}
+
+BOOL PRIVATE ID(char** argv, int argc)
+{
+	isShowingSelectedSkill = !isShowingSelectedSkill;
+	server->GameStringf("Show skill id %s", isShowingSelectedSkill ? "Enabled" : "Disabled");
+	
+	return TRUE;
+}
+
+
+BOOL PRIVATE ToggleHotkey(char** argv, int argc)
+{
+	hotkeyEnabled = !hotkeyEnabled;
+
+	if(hotkeyEnabled)
+	{
+		server->GameStringf("Hotkey enabled");
+	}
+	else
+	{
+		server->GameStringf("Hotkey disabled");
+	}
+	
+	return TRUE;
+}
+
+MODULECOMMANDSTRUCT ModuleCommands[]=
+{
+	{
+		"help",
+		OnGameCommandHelp,
+		"helpÿc0 List commands available in this module.\n"
+		"<command> helpÿc0 Shows detailed help for <command> in this module."
+	},
+	{
+		"Buff",
+		Buff,
+		"Buff"
+	},
+	{
+		"show",
+		ID,
+		"shows id of selected skill"
+	},
+	{
+		"id",
+		ID,
+		"shows id of selected skill"
+	},
+	{
+		"hotkey",
+		ToggleHotkey,
+		"Toggles hotkey (+ on keypad)"
+	},
+	{NULL}
+};
+
+BYTE EXPORT OnGameKeyDown(BYTE iKeyCode)
+{
+	if(iKeyCode == VK_DELETE)
+	{
+		tickCount = 0;
+		buff.Start();
+	}
+
+	return iKeyCode;
+}
+
+DWORD EXPORT OnGameTimerTick()
+{
+	if(tickCount++ >= tickRate)
+	{
+		buff.OnTick();
+		tickCount = 0;
+	}
+	return 0;
+}
+
+VOID EXPORT OnThisPlayerMessage(UINT nMessage, WPARAM wParam, LPARAM lParam)
+{
+	if(nMessage == PM_AFFECT)
+	{
+		if(isShowingSelectedSkill)
+		{
+			server->GameStringf("On affect: %X", lParam);
+		}
+		buff.OnAffect(lParam);
+		tickCount = 0;
+	}
+	else if(nMessage == PM_DISAFFECT)
+	{
+		if(isShowingSelectedSkill)
+		{
+			server->GameStringf("On disaffect: %X", lParam);
+		}
+		buff.OnDisAffect(lParam);
+	}
+	else if(isShowingSelectedSkill && nMessage == PM_SPELLSELECT)
+	{
+		// shock armor = 0x28
+		server->GameStringf("Selected: %02X", lParam);
+	}
+}
